fcd6399437d2fc6edebb7dcdd7eacf7a50d30eb0
Matej Minar <matej.minar@sentry.io>
2024-09-16 17:26:50 +0200

    sentry sourcemaps

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [
-		sentrySvelteKit({
-			sourceMapsUploadOptions: {
-				org: 'minarorg',
-				project: 'javascript-sveltekit',
-				url: 'https://sentry.io/'
-			}
-		}),
-		sveltekit()
-	],
-	test: {
+    plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: 'minarorg',
+            project: 'javascript-sveltekit',
+            url: 'https://sentry.io/'
+        }
+    }), sveltekit(), sentryVitePlugin({
+        org: "minarorg",
+        project: "javascript-sveltekit"
+    })],
+
+    test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+
+    build: {
+        sourcemap: true
+    }
 });
